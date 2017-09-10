dc450ff75f230aa90db6ca14fe0af2857774948c
quoctuan.cqt@gmail.com <quoctuan.cqt@gmail.com>
2017-09-10 23:23:30 +0700

    Initialize Image Upload Component.

---
+++ src/tc-img-upload/file-uploader.component.ts
@@ -0,0 +1,78 @@
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'file-uploader',
+  templateUrl: './file-uploader.component.html',
+  styleUrls: ['./file-uploader.component.scss']
+})
+export class FileUploaderComponent {
+  iconColor: string = "";
+  borderColor: string = "";
+  dragging: boolean = false;
+  loaded: boolean = false;
+  imageLoaded: boolean = false;
+  imageSrc: string = '';
+
+  @Input() activeColor: string = "green";
+  @Input() baseColor: string = "#ccc";
+  @Input() overlayColor: string = "rgba(255,255,255,0.5)";
+  @Output() getFile: EventEmitter<any> = new EventEmitter();
+
+  handleDragEnter() {
+    this.dragging = true;
+  }
+
+  handleDragLeave() {
+    this.dragging = false;
+  }
+
+  handleDrop(e) {
+    e.preventDefault();
+    this.dragging = false;
+    this.handleInputChange(e);
+  }
+
+  handleImageLoad() {
+    this.imageLoaded = true;
+    this.iconColor = this.overlayColor;
+  }
+
+  handleInputChange(e) {
+    var file = e.dataTransfer ? e.dataTransfer.files[0] : e.target.files[0];
+
+    var pattern = /image-*/;
+    var reader = new FileReader();
+
+    if (!file.type.match(pattern)) {
+      alert('invalid format');
+      return;
+    }
+
+    this.loaded = false;
+
+    reader.onload = this._handleReaderLoaded.bind(this);
+    reader.readAsDataURL(file);
+  }
+
+  _handleReaderLoaded(e) {
+    var reader = e.target;
+    this.imageSrc = reader.result;
+    this.loaded = true;
+    this.getFile.emit(reader.result);
+  }
+
+  _setActive() {
+    this.borderColor = this.activeColor;
+    if (this.imageSrc.length === 0) {
+      this.iconColor = this.activeColor;
+    }
+  }
+
+  _setInactive() {
+    this.borderColor = this.baseColor;
+    if (this.imageSrc.length === 0) {
+      this.iconColor = this.baseColor;
+    }
+  }
+
+}
